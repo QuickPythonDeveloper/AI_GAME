b6bed330c449418c0027ba98899114958f121192
mohsenamoon <dramatic225@gmail.com>
2023-03-17 15:55:27 +0330

    Added model based policy with value iteration and tested maps 1 and 2

---
+++ src/python_client/client_main.py
@@ -0,0 +1,22 @@
+import datetime
+
+from base import BaseAgent, Action
+from model_based_policy import ModelBasedPolicy
+
+
+class Agent(BaseAgent):
+    # actions = [Action.RIGHT] + [Action.TELEPORT] * 100
+    def do_turn(self) -> Action:
+        now1 = datetime.datetime.now()
+        phase3 = ModelBasedPolicy(self)
+        # print(self.agent_scores)
+        # phase3 = ReinforceLearning(self)
+        action = phase3.main()
+        now2 = datetime.datetime.now()
+        print(f'total_seconds: {(now2 - now1).total_seconds()}')
+        return action
+
+
+if __name__ == '__main__':
+    data = Agent().play()
+    print("FINISH : ", data)
